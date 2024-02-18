f72a86ce706d6ff1d73820a6ef0c9395ae43d86f
leslieDLcy <lesliechen666@outlook.com>
2024-02-18 13:30:24 +0000

    updated params formatting

--- params.py
+++ params.py
@@ -20,30 +20,17 @@ class Params:
     
     Iref = 0.16
     c=2
-
     V_ave = 8.9
     V_ref = 50
-
+    rotor_diameter = 50
+    
     # to compute wind loads Eq.(2) of paper Moira Di Paolo
-
     p = 1.225 # air density
     cd = 0.5 # drag coefficient
+
     # area of elements
-    An = np.array([0.496861728, 0.295312851, 0.282306657, 0.269300464, 0.25629427, 0.243288077, 0.229559317, 0.216553123, 0.20354693, 0.190540736, 0.177534542])
+    A_cross = np.array([0.496861728, 0.295312851, 0.282306657, 0.269300464, 0.25629427, 0.243288077, 0.229559317, 0.216553123, 0.20354693, 0.190540736, 0.177534542])
 
-    # for computation of F and M only
-    # plus 1 at the end
-    # area_com = np.array([0.496861728, 0.295312851, 0.282306657, 0.269300464, 0.25629427, 0.243288077, 0.229559317, 0.216553123, 0.20354693, 0.190540736, 0.177534542, 0.177534542])
-
-    # average height of the elements, nodes at the middle
-    # area_ave = np.array([0.39608729, 0.28880975, 0.27580356, 0.26279737, 0.24979117, 0.2364237 , 0.22305622, 0.21005003, 0.19704383, 0.18403764, 0.17753454])
-
-
-    # plus one height of the hub
-    # heights_com = np.array([42.55, 45.434, 48.318, 51.202, 54.086, 56.97, 59.854, 62.738, 65.622, 68.506, 71.39, 75.3])
-
-
-    rotor_diameter = 50
 
 
     # 15 elements for the tower and 15 nodes (minus the zero height at the bottom) for the tower
